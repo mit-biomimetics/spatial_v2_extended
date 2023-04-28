439ef081c829ceea91e3b97037cf8ea0f45314f6
matt <mchigs41@gmail.com>
2023-04-28 14:22:31 -0400

    Make get link position compatible wth casadi symbolics

--- kinematics/get_link_position.m
+++ kinematics/get_link_position.m
@@ -5,16 +5,25 @@ function [p_link,R_link,X0_link] = get_link_position( model, q, link_idx)
 % the given joint configuration "q".
 
 Xup = get_spatial_transforms(model, q);
-
-
 X0{model.NB_fb} = Xup{model.NB_fb};
+nb_pos = length(model.pos_idx);
+
+switch class(q)
+    case 'double'
+        p_link  = zeros(nb_pos*length(link_idx),1);
+    case 'casadi.SX'
+        p_link  = casadi.SX(nb_pos*length(link_idx),1);
+    case 'casadi.MX'
+        p_link  = casadi.MX(nb_pos*length(link_idx), 1);
+    otherwise
+        error('Invalid variable type for "q"')
+end
 
 for i = (model.NB_fb + 1):model.NB
     X0{i} = Xup{i} * X0{model.parent(i)};                      % propagate xform from origin
     [R0{i}, p0{i}] = plux(X0{i});                                % rotation from origin, translation from origin
 end
 
-nb_pos = length(model.pos_idx);
 for i = 1:length(link_idx)
     indx = (nb_pos*i-(nb_pos-1)):(nb_pos*i);
     p_link(indx) = p0{link_idx(i)}(model.pos_idx);
